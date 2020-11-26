fc0d617d682d0b9b3181ebb47772242efff816c1
Paramjit Singh Baweja <paramjitsinghj0@gmail.com>
2020-11-26 23:57:48 +0530

    bio now necessary

--- public/js/bio.js
+++ public/js/bio.js
@@ -4,16 +4,24 @@ if(init!=undefined){
     document.querySelector('#textareabox').value=init
 }
 
-function getText() {
+function getText() 
+{
     var text = document.getElementById("textareabox").value;
-    fetch('/process/bio?bio='+text).then((response)=>{
-        response.json().then((data) => {
-            if (data.error) {
-                messageOne.textContent = data.error
-                window.location.pathname = '/login'
-            } else {
-                window.location.pathname = '/profile'
-            }
+    if(text==="")
+    {
+        alert("Please write a bio")
+    }
+    else
+    {
+        fetch('/process/bio?bio='+text).then((response)=>{
+            response.json().then((data) => {
+                if (data.error) {
+                    messageOne.textContent = data.error
+                    window.location.pathname = '/login'
+                } else {
+                    window.location.pathname = '/profile'
+                }
+            })
         })
-    })
+    }
 }
